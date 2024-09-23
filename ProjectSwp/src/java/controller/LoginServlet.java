ef67a628809a5baa0bd6d380c04edbe44fdb7a24
LanPH2710 <144119135+LanPH2710@users.noreply.github.com>
2024-09-24 00:19:01 +0700

    Add files via upload

---
+++ ProjectSwp/src/java/controller/LoginServlet.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dal.LoginDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.Random;
+import model.Account;
+import model.GoogleAccount;
+
+/**
+ *
+ * @author Sonvu
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String code = request.getParameter("code");
+        GoogleLogin gg = new GoogleLogin();
+        String accessToken = gg.getToken(code);
+        GoogleAccount ggAcount = gg.getUserInfo(accessToken);
+        System.out.println(ggAcount);
+        String firstName = ggAcount.getGiven_name();
+        String lastName = ggAcount.getFamily_name();
+        String email = ggAcount.getEmail();
+        String picture = ggAcount.getPicture();
+        int randomNum = (int) (Math.random() * 101); // 0 to 100
+        String password = "1234567a";
+
+        LoginDAO login = new LoginDAO();
+        Account account = login.getByEmail(email);
+
+        if (account == null) {
+            login.inserUserByEmail("123", email, password, firstName, lastName, "", email, "", "");
+            Cookie c_user = new Cookie("user", email);
+            Cookie c_pass = new Cookie("pass", password);
+            c_user.setMaxAge(3600 * 24 * 30);
+            c_pass.setMaxAge(3600 * 24 * 30);
+            response.addCookie(c_user);
+            response.addCookie(c_pass);
+            response.sendRedirect("home.jsp");
+        } else {
+            login.getByEmail(email);
+            Cookie c_user = new Cookie("user", email);
+            Cookie c_pass = new Cookie("pass", password);
+            c_user.setMaxAge(3600 * 24 * 30);
+            c_pass.setMaxAge(3600 * 24 * 30);
+            response.addCookie(c_user);
+            response.addCookie(c_pass);
+            
+            response.sendRedirect("home.jsp");
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}

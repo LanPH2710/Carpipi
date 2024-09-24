46f0cca09ebd5e9479642657b69f1bcd11781993
LanPH2710 <144119135+LanPH2710@users.noreply.github.com>
2024-09-24 11:35:42 +0700

    Add files via upload

--- src/java/controller/valiOtpServlet.java
+++ src/java/controller/valiOtpServlet.java
@@ -2,8 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package controller;
 
+import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -11,46 +13,75 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Account;
-import dal.AccountDAO;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
- * @author Dan
+ * @author ADMIN
  */
-@WebServlet(name = "RegisterController", urlPatterns = {"/register"})
-public class RegisterController extends HttpServlet {
+@WebServlet("/ValidateOtp")
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+public class valiOtpServlet extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+       
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		int value=Integer.parseInt(request.getParameter("otp"));
+		HttpSession session=request.getSession();
+		int otp=(int)session.getAttribute("otp");
+		
+		
+		
+		RequestDispatcher dispatcher=null;
+		
+		
+		if (value==otp) 
+		{
+			
+				request.setAttribute("email", request.getParameter("email"));
+				request.setAttribute("status", "success");
+			  dispatcher=request.getRequestDispatcher("newPassword.jsp");
+			dispatcher.forward(request, response);
+			
+		}
+		else
+		{
+			request.setAttribute("message","wrong otp");
+			
+		   dispatcher=request.getRequestDispatcher("EnterOtp.jsp");
+			dispatcher.forward(request, response);
+		
+		}
+		
+	}
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet RegisterController</title>");
+            out.println("<title>Servlet valiOtpServlet</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet RegisterController at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet valiOtpServlet at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -58,52 +89,25 @@ public class RegisterController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
 
-        request.getRequestDispatcher("register.jsp").forward(request, response);
-    }
-
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String userName = request.getParameter("userName");
-        String password = request.getParameter("password");
-        String firstName = request.getParameter("firstName");
-        String lastName = request.getParameter("lastName");
-        String gender = request.getParameter("gender");
-        String email = request.getParameter("email");
-        String mobile = request.getParameter("mobile");
-        String address = request.getParameter("address");
-
-//        if (!password.equals(repassword)) {
-//            request.setAttribute("message", "Passwords do not match");
-//            request.getRequestDispatcher("Register.jsp").forward(request, response);
-//        }
-        AccountDAO dao = new AccountDAO();
-        Account accRegister = dao.checkEmailExists(email);
-        if (accRegister == null) {
-            dao.insertAccount(new Account(userName, password, firstName, lastName, gender, email, mobile, address));
-            request.setAttribute("message", "Registration successful. Please log in.");
-            request.getRequestDispatcher("login.jsp").forward(request, response); // Forward sang trang login cùng với message
-        } else {
-            request.setAttribute("message", "Email is already registered.");
-            request.getRequestDispatcher("Register.jsp").forward(request, response);
-        }
-
+    throws ServletException, IOException {
+        processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override

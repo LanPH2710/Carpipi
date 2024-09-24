349f93a7601b43d6d9777d18f8df3d46ee8f06b2
LanPH2710 <144119135+LanPH2710@users.noreply.github.com>
2024-09-24 11:20:13 +0700

    reset pw

---
+++ src/java/dal/AccountDAO.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import context.DBContext;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Account;
+
+/**
+ *
+ * @author tuana
+ */
+public class AccountDAO extends DBContext {
+
+    public void insertAccount(Account acc) {
+        try {
+            String sql = "INSERT INTO account "
+                    + "(userName, password, firstName, lastName, gender, email, mobile, address, roleId) "
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement stm = connection.prepareStatement(sql);
+
+            // Bắt đầu từ vị trí 1
+            stm.setString(1, acc.getUserName());
+            stm.setString(2, acc.getPassword());
+            stm.setString(3, acc.getFirstName());
+            stm.setString(4, acc.getLastName());
+            stm.setString(5, acc.getGender());
+            stm.setString(6, acc.getEmail());
+            stm.setString(7, acc.getMobile());
+            stm.setString(8, acc.getAddress());
+            stm.setInt(9, 4); 
+
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+
+    public Account checkEmailExists(String email) {
+        String sql = "SELECT * FROM account WHERE email = ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, email);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                return new Account(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getString(9),
+                        rs.getInt(10)
+                );
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        AccountDAO add = new AccountDAO();
+
+    }
+}

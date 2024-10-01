80be4215545a19b97ae83d112372a8786c1a5130
sonvn86 <sonvusonvu11@gmail.com>
2024-10-01 13:07:08 +0700

    product list

--- src/java/controller/ProductListMarketingServlet.java
+++ src/java/controller/ProductListMarketingServlet.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package controller;
 
 import dal.BrandDAO;
@@ -13,7 +14,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.List;
 import model.Brand;
 import model.Product;
@@ -23,45 +23,35 @@ import model.Segment;
  *
  * @author Sonvu
  */
-public class ProductListForMaketingServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+public class ProductListMarketingServlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            ProductDAO pDao = new ProductDAO();
-            BrandDAO bDao = new BrandDAO();
-            SegmentDAO sDao = new SegmentDAO();
-            
-            List<Product> productList = pDao.getAllProducts();
-            request.setAttribute("productList", productList);
-
-//        ArrayList<Category> cl = pDao.;
-//        request.setAttribute("category", cl);
-            List<Brand> brandList = bDao.getAllBrand();
-            request.setAttribute("brandList", brandList);
-            
-            List<Segment> segmentList = sDao.getAllSegment();
-            request.setAttribute("segmentList", segmentList);
-
-            request.getRequestDispatcher("product_list_maketing.jsp").forward(request, response);
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ProductListMarketingServlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ProductListMarketingServlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -69,13 +59,25 @@ public class ProductListForMaketingServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+    throws ServletException, IOException {
+        ProductDAO pDao = new ProductDAO();
+        BrandDAO bDao = new BrandDAO();
+        SegmentDAO sDao = new SegmentDAO();
 
-    /**
+        List<Product> productList = pDao.getAllProducts();
+        request.setAttribute("productList", productList);
+
+        List<Brand> brandList = bDao.getAllBrand();
+        request.setAttribute("brandList", brandList);
+
+        List<Segment> segmentList = sDao.getAllSegment();
+        request.setAttribute("segmentList", segmentList);
+        
+        request.getRequestDispatcher("product_list_maketing.jsp").forward(request, response);
+    } 
+
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -83,13 +85,12 @@ public class ProductListForMaketingServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override

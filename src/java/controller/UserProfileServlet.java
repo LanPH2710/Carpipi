8698ec055f85f582bfd92d7022182687c5ec3304
jindy12 <tuantvhe180495@fpt.edu.vn>
2024-10-07 13:34:32 +0700

    update tuantv

--- src/java/controller/UserProfileServlet.java
+++ src/java/controller/UserProfileServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dal.AccountDAO;
@@ -30,35 +29,38 @@ import model.Account;
         maxFileSize = 1024 * 1024 * 5, // 5 MB
         maxRequestSize = 1024 * 1024 * 10 // 10 MB
 )
-public class UserProfileServlet extends HttpServlet{
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+public class UserProfileServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet EditUserProfileServlet</title>");  
+            out.println("<title>Servlet EditUserProfileServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet EditUserProfileServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet EditUserProfileServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -66,7 +68,7 @@ public class UserProfileServlet extends HttpServlet{
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         AccountDAO adao = new AccountDAO();
         HttpSession session = request.getSession();
         Account acc = (Account) session.getAttribute("account");
@@ -76,10 +78,11 @@ public class UserProfileServlet extends HttpServlet{
         String role = rdao.getRoleNameById(user.getRoleId());
         session.setAttribute("role", role);
         request.getRequestDispatcher("UserProfile.jsp").forward(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -104,6 +107,13 @@ public class UserProfileServlet extends HttpServlet{
         String gender = request.getParameter("gender");
         String mobile = request.getParameter("mobile");
         String address = request.getParameter("address");
+        //Verifile moblie
+        boolean isMobileValid = adao.isValidMobile(mobile);
+        if (!isMobileValid) {
+            request.setAttribute("errorMessage", "Số điện thoại phải có 10 số.");
+            request.getRequestDispatcher("UserProfile.jsp").forward(request, response);
+            return;
+        }
 
         // Xử lý upload avatar nếu có file mới
         Part file = request.getPart("avatar");
@@ -118,8 +128,7 @@ public class UserProfileServlet extends HttpServlet{
             }
 
             // Lưu file
-            try (InputStream is = file.getInputStream();
-                 FileOutputStream fos = new FileOutputStream(uploadPath)) {
+            try (InputStream is = file.getInputStream(); FileOutputStream fos = new FileOutputStream(uploadPath)) {
                 byte[] buffer = new byte[1024];
                 int bytesRead;
                 while ((bytesRead = is.read(buffer)) != -1) {
@@ -133,11 +142,13 @@ public class UserProfileServlet extends HttpServlet{
 
         // Cập nhật account
         adao.editAccount(userName, password, firstName, lastName, gender, email, mobile, address, roleId, avatar, userId);
-        response.sendRedirect("userprofile");
+        request.setAttribute("errorMessage", "Thay đổi thành công");
+        request.getRequestDispatcher("UserProfile.jsp").forward(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override

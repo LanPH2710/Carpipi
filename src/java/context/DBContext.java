84ec1bb61a79b8574308e4ff3651019c2e599682
hieu <155555185+thanhhieule@users.noreply.github.com>
2024-10-31 16:25:40 +0700

    hieu

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -15,7 +15,7 @@ public class DBContext {
             String user = "root"; // Your MySQL username
             String pass = "12345"; // Your MySQL password
 
-            String url = "jdbc:mysql://localhost:3306/carpipi"; // MySQL URL format - your schema name
+            String url = "jdbc:mysql://localhost:3306/carpipi1"; // MySQL URL format - your schema name
             Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL driver class
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException e) {
